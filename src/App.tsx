bd64bd9f11d080f384bbfaa612de6476fae0e165
Kevin Mason <krmason2020@gmail.com>
2022-03-27 11:23:33 -0700

    @13:00:00

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactNode, ReactElement } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { title } from "process";
+
+//conventional props
+
+//{title?:string} the ? makes a param optional
+
+const Heading = ({ title }: { title: string }) => {
+  return <h1>{title}</h1>;
+};
+
+const HeadingWithContent = ({
+  children,
+}: {
+  children: ReactNode;
+}): ReactElement => {
+  return <h1>{children}</h1>;
+};
+
+//React node used for tags
+//Returning react element is not necessary because its assumed
+
+//default props
+
+const defaultContainerProps = {
+  heading: <strong>My Heading</strong>,
+};
+
+type ContainerProps = {
+  children: ReactNode;
+} & typeof defaultContainerProps;
+
+function Container({ heading, children }: ContainerProps): ReactElement {
+  return (
+    <div>
+      <h1>{heading}</h1>
+      <h1>{children}</h1>
+    </div>
+  );
+}
+
+Container.defaultProps = defaultContainerProps;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title="Hello there" />
+      <HeadingWithContent>
+        <strong>Hi</strong>
+      </HeadingWithContent>
+      <Container>Foo</Container>
     </div>
   );
 }
